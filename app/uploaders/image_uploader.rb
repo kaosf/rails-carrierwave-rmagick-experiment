18656b11affac42a25fa2a509d940c7461505ea3
ka <ka.kaosf@gmail.com>
2014-11-21 22:38:29 +0900

    Enable extension white list feature

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -46,6 +46,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   %w(jpg jpeg gif png)
   # end
 
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
